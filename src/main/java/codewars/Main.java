51215ba887fa3377301fb6ce8b16f73a1adfc1eb
Yaroslav <yaroslav_lav1@bk.ru>
2022-11-23 22:23:41 +0300

    SumOfPositive

--- src/main/java/codewars/Main.java
+++ src/main/java/codewars/Main.java
@@ -3,8 +3,14 @@ package codewars;
 public class Main {
     public static void main(String[] args) {
 
+        int[] arr;
+        arr = new int[3];
 
-        System.out.println(1%2);
+        arr[0]=3;
+        arr[1]=5;
+        arr[2]=-4;
+
+        System.out.println(SumOfPositive.sum(arr));
 
     }
 
--- src/main/java/codewars/SumOfPositive.java
+++ src/main/java/codewars/SumOfPositive.java
@@ -1,4 +1,25 @@
 package codewars;
 
+import java.util.Arrays;
+
 public class SumOfPositive {
+    public static int sum(int[] arr) {
+//        int result = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] > 0) {
+//                result += arr[i];
+//            }
+//        }
+        return result;
+        int result = 0;
+        for (int i : arr) {
+            if (i > 0) {
+                result += i;
+            }
+        }
+        return result;
+
+//        return Arrays.stream(arr).filter(v -> v>0).sum();
+
+    }
 }
