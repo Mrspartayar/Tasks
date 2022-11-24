175df5151910e7652361acf7c266a5aef6de8593
Yaroslav <yaroslav_lav1@bk.ru>
2022-11-24 21:03:19 +0300

    ReturnNegative

--- src/main/java/codewars/ReturnNegative.java
+++ src/main/java/codewars/ReturnNegative.java
@@ -1,4 +1,16 @@
 package codewars;
 
 public class ReturnNegative {
+
+    public static int makeNegative(final int x) {
+        if (x > 0) {
+            return x * -1;
+        } else if (x < 0) {
+            return x;
+        }
+        return 0;
+        //return -abs(x);
+        //return (x < 0) ? x : -x;
+    }
+
 }
