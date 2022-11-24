6983ff7a5e72049d54eb96d8991867725c3895fe
Yaroslav <yaroslav_lav1@bk.ru>
2022-11-24 21:23:54 +0300

    WillYouMakeIt

---
+++ src/main/java/codewars/WillYouMakeIt.java
@@ -0,0 +1,8 @@
+package codewars;
+
+public class WillYouMakeIt {
+    static boolean zeroFuel(double distanceToPump, double mpg, double fuelLeft) {
+        return distanceToPump <= mpg * fuelLeft;
+    }
+}
+
